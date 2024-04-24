964a27e6ce472a51020d0089e451c8076ac75300
mohan <mohan@mohanvadivel.com>
2024-04-24 07:20:55 +0530

    compressed static image. added svelte enhanced images plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import svgLoader from 'vite-svg-loader'; // Vite plugin: loads SVG files as components
+import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
-	plugins: [sveltekit(), svgLoader({ defaultImport: 'raw' })],
+	plugins: [enhancedImages(), sveltekit(), svgLoader({ defaultImport: 'raw' })],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
